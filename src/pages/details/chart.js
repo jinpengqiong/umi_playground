f4b45028392a21bb2d02b669c7282b7cfc609ea6
Peng Jin(金鹏) <peng.jin@symbio.com>
2020-11-25 18:16:32 +0800

    added sum comp

--- src/pages/details/chart.js
+++ src/pages/details/chart.js
@@ -6,52 +6,97 @@ import styles from './index.less';
 export const ChartComponent = props => {
   useEffect(() => {
     const ctx = document.getElementById('myChart')?.getContext('2d');
-    const myChart =  new Chart(ctx, {
-    type: 'line',
-    data: {
-      labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-      datasets: [
-        {
-          label: '# of Votes',
-          data: [12, 19, 3, 5, 2, 3],
-          backgroundColor: [
-            'rgba(255, 99, 132, 0.2)',
-            'rgba(54, 162, 235, 0.2)',
-            'rgba(255, 206, 86, 0.2)',
-            'rgba(75, 192, 192, 0.2)',
-            'rgba(153, 102, 255, 0.2)',
-            'rgba(255, 159, 64, 0.2)',
-          ],
-          borderColor: [
-            'rgba(255, 99, 132, 1)',
-            'rgba(54, 162, 235, 1)',
-            'rgba(255, 206, 86, 1)',
-            'rgba(75, 192, 192, 1)',
-            'rgba(153, 102, 255, 1)',
-            'rgba(255, 159, 64, 1)',
-          ],
-          borderWidth: 1,
-        },
-      ],
-    },
-    options: {
-      scales: {
-        yAxes: [
+    const myChart = new Chart(ctx, {
+      type: 'line',
+      data: {
+        labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+        datasets: [
           {
-            ticks: {
-              beginAtZero: true,
-            },
+            label: 'Agent',
+            backgroundColor: [
+              'rgba(255, 99, 132, 0.2)',
+              'rgba(54, 162, 235, 0.2)',
+              'rgba(255, 206, 86, 0.2)',
+              'rgba(75, 192, 192, 0.2)',
+              'rgba(153, 102, 255, 0.2)',
+              'rgba(255, 159, 64, 0.2)',
+            ],
+            borderColor: [
+              'rgba(255, 99, 132, 1)',
+              'rgba(54, 162, 235, 1)',
+              'rgba(255, 206, 86, 1)',
+              'rgba(75, 192, 192, 1)',
+              'rgba(153, 102, 255, 1)',
+              'rgba(255, 159, 64, 1)',
+            ],
+            data: [2, 12, -12, -5, -2, 7, 0],
+            fill: false,
+          },
+          {
+            label: 'Caller',
+            fill: false,
+            backgroundColor: [
+              'rgba(255, 99, 132, 0.2)',
+              'rgba(54, 162, 235, 0.2)',
+              'rgba(255, 206, 86, 0.2)',
+              'rgba(75, 192, 192, 0.2)',
+              'rgba(153, 102, 255, 0.2)',
+              'rgba(255, 159, 64, 0.2)',
+            ],
+            borderColor: [
+              'rgba(255, 99, 132, 1)',
+              'rgba(54, 162, 235, 1)',
+              'rgba(255, 206, 86, 1)',
+              'rgba(75, 192, 192, 1)',
+              'rgba(153, 102, 255, 1)',
+              'rgba(255, 159, 64, 1)',
+            ],
+            data: [9, 3, 2, -4, -3, -10, 3],
           },
         ],
       },
-    },
-  });
+      options: {
+        responsive: true,
+        title: {
+          display: true,
+          text: 'Chart.js Line Chart',
+        },
+        tooltips: {
+          mode: 'index',
+          intersect: false,
+        },
+        hover: {
+          mode: 'nearest',
+          intersect: true,
+        },
+        scales: {
+          xAxes: [
+            {
+              display: false,
+              scaleLabel: {
+                display: true,
+                labelString: 'Month',
+              },
+            },
+          ],
+          yAxes: [
+            {
+              display: true,
+              scaleLabel: {
+                display: true,
+                labelString: 'Value',
+              },
+            },
+          ],
+        },
+      },
+    });
     console.log('ctx', ctx)
   }, [])
 
   return (
     <div>
-      <canvas id="myChart" width="400" height="400"></canvas>
+      <canvas id="myChart" width='500' height="400"></canvas>
     </div>
   );
 };
---
+++ src/pages/details/summary.js
@@ -0,0 +1,115 @@
+import { ChartComponent } from './chart';
+import ProCard from '@ant-design/pro-card';
+import { Row, Col } from 'antd';
+const { Divider } = ProCard;
+// import styles from './index.less';
+// import { formatMessage } from 'umi-plugin-locale';
+
+export const SummaryPage = props => {
+  return (
+      <ProCard style={{ marginTop: 8, minHeight: '30vh' }} gutter={8}>
+        <ProCard colSpan={{ xs: 5, sm: 6, md: 6, lg: 6, xl: 6 }} layout="left">
+          <Row>
+            <Col span={24}>
+              <h3>Contact Summary</h3>
+            </Col>
+          </Row>
+          <br />
+          <Row>
+            <Col span={10}>
+              <strong>Date/Time:</strong>
+            </Col>
+            <Col span={14}>October 210,2020 10:30 AM UTC</Col>
+          </Row>
+          <br />
+          <Row>
+            <Col span={10}>
+              <strong>Entity Recognizer Name:</strong>
+            </Col>
+            <Col span={14}>Micheal</Col>
+          </Row>
+          <br />
+          <Row>
+            <Col span={10}>
+              <strong>Language Code:</strong>
+            </Col>
+            <Col span={14}>en-US</Col>
+          </Row>
+          <br />
+          <Row>
+            <Col span={10}>
+              <strong>Agent Sentiment:</strong>
+            </Col>
+            <Col span={14}>
+              <div>Overall negative;</div>
+              <div>trending down</div>
+            </Col>
+          </Row>
+          <br />
+        </ProCard>
+        <Divider />
+        <ProCard colSpan={{ xs: 5, sm: 6, md: 6, lg: 6, xl: 6 }} layout="left">
+          <Row>
+            <Col span={24}>
+              <h3>Source Information</h3>
+            </Col>
+          </Row>
+          <br />
+          <Row>
+            <Col span={10}>
+              <strong>Type:</strong>
+            </Col>
+            <Col span={14}>Voice Transcription</Col>
+          </Row>
+          <br />
+          <Row>
+            <Col span={10}>
+              <strong>Job Name:</strong>
+            </Col>
+            <Col span={14}>2020102632523535.mp3</Col>
+          </Row>
+          <br />
+          <Row>
+            <Col span={10}>
+              <strong>File Format:</strong>
+            </Col>
+            <Col span={14}>mp3</Col>
+          </Row>
+          <br />
+          <Row>
+            <Col span={10}>
+              <strong>Call Duration:</strong>
+            </Col>
+            <Col span={14}>77:04</Col>
+          </Row>
+          <br />
+          <Row>
+            <Col span={10}>
+              <strong>Sample Rate:</strong>
+            </Col>
+            <Col span={14}>8000</Col>
+          </Row>
+          <br />
+          <Row>
+            <Col span={10}>
+              <strong>Custom Vocabulary:</strong>
+            </Col>
+            <Col span={14}>77:04</Col>
+          </Row>
+          <br />
+          <Row>
+            <Col span={10}>
+              <strong>Word Accuracy:</strong>
+            </Col>
+            <Col span={14}>96.7%</Col>
+          </Row>
+          <br />
+        </ProCard>
+        <Divider />
+        <ProCard colSpan={{ xs: 14, sm: 12, md: 12, lg: 12, xl: 12 }} layout="center">
+          <ChartComponent />
+        </ProCard>
+      </ProCard>
+  );
+};
+
